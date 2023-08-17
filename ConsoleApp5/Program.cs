60d92890da3cabc779cc86ddb9e0aaa8f91d68bd
Khalilnkh <khalilnkh@code.edu.az>
2023-08-17 13:03:16 +0400

    Created

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathHelper mt=new MathHelper();
+            Console.WriteLine(mt.PrintValue());
+        }
+    }
+    public class MathHelper
+    {
+        public int PrintValue(int a,int b)
+        {
+            return a + b;
+
+        }
+        public int PrintValue(bool a)
+        {
+            if (a==true)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+        public void PrintValue(string a, string b)
+        {
+            Console.WriteLine(a+b);
+        }
+        public int PrintValue(int a, int b,int c)
+        {
+            return a * b * c;
+
+        }
+    }
+}
+
