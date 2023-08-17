1725c076b235164bb273340b22afa101382c439e
Khalilnkh <khalilnkh@code.edu.az>
2023-08-17 15:46:43 +0400

    updated

---
+++ ConsoleApp5/MathHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    public class MathHelper
+    {
+        public int PrintValue(int a, int b)
+        {
+            return a + b;
+
+        }
+        public int PrintValue(bool a)
+        {
+            if (a == true)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+        public void PrintValue(string a, string b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public int PrintValue(int a, int b, int c)
+        {
+            return a * b * c;
+
+        }
+    }
+}
